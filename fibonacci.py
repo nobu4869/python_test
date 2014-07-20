a572bdad5045523e914587c082164264a617196e
yamakido.n <yamakido.n@gmail.com>
2014-07-20 09:22:51 +0900

    change fibonacci func +return

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,14 @@
 __author__ = 'yamakido.n'
 
 def fib(n):
+    result = []
     a,b = 0,1
     print 'a=' + a.__str__()
     print 'b=' , b.__str__()
     while b <n:
         print b
+        result.append(a)
         a,b=b,a+b
 
-fib(100)
+f100 = fib(100)
+f100
